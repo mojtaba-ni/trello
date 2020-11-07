d7c458f9c9feabdb912cf0093e25fec21c00ca6f
Mojtaba <Mojtaba>
2020-11-07 05:30:35 -0800

    initializing lexer(Scanner) of the project

---
+++ lexer.py
@@ -0,0 +1,9 @@
+class Lexer:
+    def __init__(self):
+        self.lexer = LexerGenerator()  # This is a requiment will be added later
+
+    """
+        //TODO
+        we will add some tokens here later
+    """
+
