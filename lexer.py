f0b8cbaea0f0e2c6be4c4cda4b5a3db89f6eea99
Mojtaba <Mojtaba>
2020-11-15 08:01:57 -0800

    some tokens added annd installing rply

--- lexer.py
+++ lexer.py
@@ -1,9 +1,18 @@
+from rply import LexerGenerator
+
+
 class Lexer:
     def __init__(self):
         self.lexer = LexerGenerator()  # This is a requiment will be added later
 
+    # Toekens for operators
+    self.lexer.add("SUM", r"\+")
+    self.lexer.add("SUB", r"\-")
+    self.lexer.add("MUL", r"\*")
+    self.lexer.add("DIV", r"\/")
+
     """
         //TODO
-        we will add some tokens here later
+        tokens will be completed later on
     """
 
